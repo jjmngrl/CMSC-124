334e06f003fe8bde31aff74ca9e257ff8d3c7682
Dan Victor Gapaz <dogapaz@up.edu.ph>
2024-11-30 15:51:15 +0800

    chore: enable reading of following keywords:  "I", "I IZ"

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -15,7 +15,7 @@ Reads a LOLCODE file and removes comments and empty lines.
 """
 
 def read():
-    file = r"test case/07_ifelse.lol"
+    file = r"test case/10_functions.lol"
     if file.endswith(".lol"):
         with open(file, 'r') as f:
             lines = f.readlines()
@@ -90,6 +90,7 @@ def classifier(lines):
             r"SUM OF",
             r"WON OF",
             r"YA RLY",
+            r"I IZ",
             r"MEBBE",
             r"NUMBR",
             r"TROOF",
@@ -110,7 +111,8 @@ def classifier(lines):
             r"TIL",
             r"AN",
             r"YR",
-            r"R"
+            r"R",
+            r"IT"
         ],
         # Literals
         "NUMBR": r"^-?\d+$",
