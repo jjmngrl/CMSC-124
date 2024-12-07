d6cd408893a1905136edc44b3ce2b4ada93df9b8
Dan Victor Gapaz <dogapaz@up.edu.ph>
2024-12-07 11:51:21 +0800

    Added Semantics main file and semantics_function

---
+++ semantics.py
@@ -0,0 +1,198 @@
+from lexical_analyzer import *
+from syntax_functions import program_checker
+from syntax_functions import variable_section_checker
+from syntax_functions import semantics_functions
+from syntax_functions import visible_statement_checker
+from syntax_functions import assignment_checker
+
+def semantics(code_block, classified_tokens):
+    
+    #Check semantics of Variable section
+    wazzup_key = None
+    buhbye_key = None
+    variable_section_exists = False
+
+    """Check if there is a variable section"""
+    for line_num, tokens in code_block.items():
+        for token, token_type in tokens:
+            if token_type == "KEYWORD" and token == "WAZZUP":
+                variable_section_exists = True
+                wazzup_key = line_num
+            if token_type == "KEYWORD" and token == "BUHBYE":
+                variable_section_exists = True
+                buhbye_key = line_num
+    
+    #raise error when there is no wazzup or buhbye
+    if wazzup_key == None and variable_section_exists:
+        raise Exception("ERROR: Invalid Variable Section due to missing WAZZUP")
+    if buhbye_key == None and variable_section_exists:
+        raise Exception("ERROR: Invalid Variable Section due to missing BUHBYE")
+        
+
+    if variable_section_exists:
+        #Check if variable section is valid
+        if variable_section_checker.variable_section_checker(code_block, classified_tokens):
+        #Create a new dictionary without the variable section to check what kind of statement are the remaining code
+            new_code_block = {}
+            for key, value in code_block.items():
+                if buhbye_key is not None and key > buhbye_key:
+                    new_code_block[key] = value  # Retain lines after BUHBYE
+            
+            code_block = new_code_block
+        
+    print("Variable exist result: ", variable_section_exists)
+    print("Updated symbol table after checking variable section: \n", semantics_functions.symbols)
+    statement_flag = False
+
+
+    print("\nCODE BLOCK: \n", code_block)
+    """ Check succeeding line and determine the type of statement"""
+    current_line = list(code_block.keys())[0] #This is the first line after the variable declaration
+    while current_line in code_block:
+        tokens = code_block[current_line]
+        print(f"\nToken being checked in line {current_line}: ", tokens)
+
+        if current_line < list(code_block.keys())[-1]:
+            catch_wtf = code_block[current_line+1]
+            # print(catch_wtf)
+            switch_exist = False
+            if catch_wtf != []:
+                switch_exist = True
+            # print(catch_wtf[0])
+        if tokens == []:
+            current_line += 1
+            continue
+        #check for print statement
+        if tokens[0][0] == "VISIBLE":
+            print("Insert print checker here")
+            visible_statement_checker.visible_statement_checker(current_line, tokens)
+            statement_flag = True
+            current_line += 1 #Move to the next line
+
+        # #catch for if-else
+        # elif tokens[0][0] == "O RLY?" and tokens[0][1] == "KEYWORD":
+        #     print(f"start of if-else block at line {current_line}")
+        #     extract_block, next_line = extract_flowcontrol_block.extract_ifelse_block(code_block, current_line)
+        #     print("Extracted block:\n", extract_block)
+        #     #Syntax analyzer
+        #     if ifelse_checker.ifelse_checker(extract_block):
+        #         print(f"valid if else block at line {current_line} to line {next_line-1}")
+        #         statement_flag = True
+        #     else:
+        #         raise Exception(f"ERROR in line {current_line}:Invalid if-else block.")
+            
+        #     #semantics
+
+        #     current_line = next_line
+
+        # #Catch switch
+        # elif switch_exist == True and catch_wtf[0][0] == "WTF?":
+        #     print(f"Start of switch-case block at line {current_line+1}")
+        #     extracted_block, next_line = extract_flowcontrol_block.extract_switch_block(code_block, current_line+1)
+        #     print("Extracted switch-case block:\n", extracted_block)
+        #     if switch_checker.switch_checker(extracted_block):
+        #         print(f"valid switch block at line {current_line+1} to line {next_line+1}")
+        #         statement_flag = True
+        #     else:
+        #         raise Exception(f"ERROR in line {current_line+1}:Invalid switch block.")
+        #     current_line = next_line  # Move to the next line after the block
+        #     # current_line += 1
+
+        # #Catch function call
+        # elif tokens[0][0] == "I IZ":
+        #     print(f"Function call at line {current_line}")
+        #     result = func_call_checker.function_call_checker(tokens, current_line)
+        #     if result == True:
+        #         print(f"Valid function call at line {current_line}")
+        #         statement_flag = True
+        #     else:
+        #         raise Exception(result)
+
+        #     current_line += 1
+
+        # #catch loops
+        # elif tokens[0][0] == "IM IN YR":
+        #     print(f"Start of loop black at line {current_line}")
+        #     extract_block, next_line = extract_flowcontrol_block.extract_loop_block(code_block, current_line)
+        #     print("Extracted loop block:\n", extract_block)
+        #     result = loop_checker.loop_checker(extract_block)
+        #     if result == True:
+        #         print(f"valid loop block at line {current_line} to line {next_line-1}")
+        #         statement_flag = True
+        #     else:
+        #         raise Exception(result)
+        #     current_line = next_line  # Move to the next line after the block
+
+        # #catch input
+        # elif tokens[0][0] == "GIMMEH":
+        #     print(f"Input at line {current_line}")
+
+        #     if gimmeh_statement_checker.gimmeh_statement_checker(tokens):
+        #         print("Valid input/gimmeh statemet")
+        #         statement_flag = True
+        #     else:
+        #         raise Exception(f"ERROR at line {current_line}: Input statements must follow this format: GIMMEH <varident>")
+
+        #     current_line += 1
+        
+        # #catch function
+        # elif tokens[0][0] == "HOW IZ I":
+        #     print(f"Start of function block at line {current_line}")
+        #     extracted_block, next_line = function_checker.extract_function_block(code_block, current_line)
+        #     print("Extracted function block:\n",extracted_block)
+        #     result = function_checker.function_checker(extracted_block)
+        #     if result == True:
+        #         print(f"Valid functin block at line {current_line} to line {next_line-1}")
+        #         statement_flag = True
+        #     else:
+        #         raise Exception(result)
+
+        #     current_line = next_line
+
+        # #catch expression
+        # elif expression_checker.expression_checker(tokens[1:], False) == True:
+        #     print("Valid expression")
+        #     current_line += 1
+        #     statement_flag = True
+            
+        #catch assignment
+        elif tokens[1][0] == "R":
+            print(f"Assignment statement at line {current_line}")
+            result =  assignment_checker.assignment_semantics(current_line, tokens)
+            if result:
+                print("Valid assignment statent")
+                statement_flag = True
+            else:
+                raise Exception(result)
+        
+            current_line += 1
+            
+        
+
+
+
+
+        
+        else:
+            print("Nah")
+            current_line += 1
+        
+    print(semantics_functions.symbols)
+    return statement_flag
+
+
+
+classified_tokens = lex_main()
+code_block_in_program = {}
+program_indices = program_checker.program_checker(classified_tokens)
+if program_indices:
+    #program is valid
+    index_of_HAI, index_of_KTHXBYE = program_indices
+    #dictionary that does not contain "HAI" and "KBYETHX"
+    code_block_in_program = {
+        k: classified_tokens[k]
+        for k in sorted(classified_tokens.keys())
+        if index_of_HAI < k < index_of_KTHXBYE
+    }
+
+semantics(code_block_in_program, classified_tokens)
--- syntax_functions/assignment_checker.py
+++ syntax_functions/assignment_checker.py
@@ -1,6 +1,10 @@
 from syntax_functions.literal_checker import literal_checker
 from syntax_functions.identifier_checker import identifier_checker
 from syntax_functions.expression_checker import expression_checker
+from syntax_functions.semantics_functions import *
+# from literal_checker import literal_checker
+# from identifier_checker import identifier_checker
+# from expression_checker import expression_checker
 
 def assignment_checker(line_num, tokens):
     flag = False  # Tracks if all VISIBLE statements are valid
@@ -9,11 +13,14 @@ def assignment_checker(line_num, tokens):
         # Check if there's a value after 'R'
         if len(tokens) < 3:
             # result.append("ERROR: Missing value after 'VISIBLE'.")
-            raise Exception ("ERROR at line {line_num}: Missing value after 'R'.")
+            raise Exception ("SYNTAX ERROR at line {line_num}: Missing value after 'R'.")
             return False
         else:
             visible_part = tokens[2:]
-            first_value, first_type = visible_part[0]
+            # first_value, first_type = visible_part[0]
+           
+
+
             # Check if it's a literal
             if len(visible_part) > 1   == True:
                 # result.append("Valid literal")
@@ -25,6 +32,9 @@ def assignment_checker(line_num, tokens):
                 if identifier_checker(visible_part[0]) == True:
                     # result.append("Valid identifier")
                     flag = True
+                    
+                    #Semantics - check if variable is in the symbol table
+                    # symbol_exists(tokens[0][0])
 
                 # Check if it's a valid expression
                 elif literal_checker(visible_part[0]):
@@ -50,3 +60,62 @@ def assignment_checker(line_num, tokens):
 
     # Return the result flag (True if valid, False if invalid)
     return flag
+
+test_case = {
+    13: [['x', 'IDENTIFIER'], ['R', 'KEYWORD'], ['SMOOSH', 'KEYWORD'], ['x', 'IDENTIFIER'], ['AN', 'KEYWORD'], ['y', 'IDENTIFIER']],
+    14: [['y', 'IDENTIFIER'], ['R', 'KEYWORD'], ['100', 'NUMBR']],
+    19: [['y', 'IDENTIFIER'], ['R', 'KEYWORD'], ['0', 'NUMBR']],
+    20: [['y', 'IDENTIFIER'], ['R', 'KEYWORD'], ['MAEK', 'KEYWORD'], ['y', 'IDENTIFIER'], ['A', 'KEYWORD'], ['TROOF', 'KEYWORD']],
+}
+
+
+
+def assignment_semantics(line_num, tokens):
+    print("assignment semantics")
+    token_index = 0
+    # print(len(tokens))
+    value_of_ass = 0  
+    type_of_ass = ""
+    var_name = ""
+    for idx in range(len(tokens)):
+        print(tokens[idx][0])
+
+        #check if the variable is declared
+        if tokens[idx][1] == "IDENTIFIER":
+            result = symbol_exists(tokens[0][0])
+            var_name = tokens[0][0]
+            # print("result" ,result) 
+            if not result:
+                raise Exception(f"Error in line {line_num}: Variable {tokens[0][0]} is not declared")
+            idx += 1
+        
+        elif tokens[idx][0] != "R" and tokens[idx][1] == "KEYWORD":
+            print("expression")
+            print(tokens[idx:])
+            #Store the value of expression to the value of the symbol
+            idx += 1
+            break
+
+        elif literal_checker(tokens[idx]):
+            #assign the value
+            update_symbol(var_name, value=tokens[idx][0], value_type=tokens[idx][1])
+            print(symbols)
+            idx += 1
+        else:
+            idx += 1
+    
+    return True
+
+
+
+
+
+
+# # Run test cases
+# for i, (line_num, tokens) in enumerate(test_case.items()):
+#     # print(i)
+#     result = assignment_semantics(line_num,tokens)
+#     print(f"Test Case {i}: {'Valid' if result == True else result}")
+#     # print(symbols)
+
+# print(symbol_table)
